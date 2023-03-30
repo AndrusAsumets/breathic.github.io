49d300e197b7a0d4cb92cb1d1d26977d7a259727
Andrus Asumets <andrus.asumets@gmail.com>
2023-03-30 11:48:43 +0300

    update waypoint v3

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 (async () => {
   const separator = 'mailto:';
   const sleep = ms => new Promise(r => setTimeout(r, ms));
-  const parseSection = waypoint => 
+  const parseSection = (waypoint) =>
     waypoint.href.split(separator)[1].split('@way.point')[0];
   const updateWatch = (section) =>
     document.querySelector('.watch-content').style.backgroundImage = `url(/static/img/${section}.png)`;
@@ -10,17 +10,16 @@
 
   while (!areWaypointsEnabled) {
     const waypoints = document.getElementsByTagName('a');
+    const sections = Array.prototype
+      .map.call(waypoints, (waypoint) => waypoint)
+      .filter(waypoint => waypoint && waypoint.href && waypoint.href.includes(separator));
 
-    if (!waypoints.length) {
+    if (!sections.length) {
       await sleep(100);
       continue;
     }
 
     areWaypointsEnabled = true;
-
-    const sections = Array.prototype
-      .map.call(waypoints, (waypoint) => waypoint)
-      .filter(waypoint => waypoint && waypoint.href && waypoint.href.includes(separator));
 
     updateWatch(parseSection(sections[sectionIndex]));
 
